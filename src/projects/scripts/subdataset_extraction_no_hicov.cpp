af6040e6fdec335c39e4fd2892109d88eb0a0a45
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2022-07-08 16:18:56 -0700

    Draft of no-hicov script

---
+++ src/projects/scripts/subdataset_extraction_no_hicov.cpp
@@ -0,0 +1,119 @@
+#include <common/cl_parser.hpp>
+#include <sequences/contigs.hpp>
+#include <experimental/filesystem>
+#include <common/logging.hpp>
+#include <sequences/seqio.hpp>
+#include <common/rolling_hash.hpp>
+#include <dbg/dbg_construction.hpp>
+#include <dbg/component.hpp>
+#include <dbg/graph_alignment_storage.hpp>
+#include <dbg/subdatasets.hpp>
+#include "dbg/graph_modification.hpp"
+
+int main(int argc, char **argv) {
+    CLParser parser({"unique=none", "output-dir=",
+                     "threads=16", "k-mer-size=", "window=2000", "base=239", "hicov=500", "debug",
+                     "reference=none", "compress", "dimer-compress=1000000000,1000000000,1", "unique-threshold=40000", "radius=1000", "bad-cov=7"},
+                    {"reads"},
+                    {"o=output-dir", "t=threads", "k=k-mer-size", "w=window"},
+                    "");
+    parser.parseCL(argc, argv);
+    if (!parser.check().empty()) {
+        std::cout << "Failed to parse command line parameters." << std::endl;
+        std::cout << parser.check() << "\n" << std::endl;
+        std::cout << parser.message() << std::endl;
+        return 1;
+    }
+
+    bool debug = parser.getCheck("debug");
+    StringContig::homopolymer_compressing = parser.getCheck("compress");
+    StringContig::SetDimerParameters(parser.getValue("dimer-compress"));
+    const std::experimental::filesystem::path dir(parser.getValue("output-dir")); //initialization of dir
+    ensure_dir_existance(dir);
+    logging::LoggerStorage ls(dir, "dbg");
+    logging::Logger logger;
+    logger.addLogFile(ls.newLoggerFile(), debug ? logging::debug : logging::trace);
+    for (size_t i = 0; i < argc; i++) {
+        logger << argv[i] << " ";
+    }
+    logger << std::endl;
+    size_t k = std::stoi(parser.getValue("k-mer-size"));
+    const size_t w = std::stoi(parser.getValue("window"));
+    double bad_cov = std::stod(parser.getValue("bad-cov"));
+    size_t unique_threshold = std::stoi(parser.getValue("unique-threshold"));
+    io::Library reads_lib = oneline::initialize<std::experimental::filesystem::path>(parser.getListValue("reads"));
+    io::Library ref_lib;
+    if(parser.getValue("reference") != "none") {
+        ref_lib = oneline::initialize<std::experimental::filesystem::path>(parser.getListValue("reference"));
+    }
+    hashing::RollingHash hasher(k, std::stoi(parser.getValue("base")));
+    size_t threads = std::stoi(parser.getValue("threads"));
+    size_t hicov = std::stoi(parser.getValue("hicov"));
+    // (Finished parsing command-line arguments.)
+
+    dbg::SparseDBG dbg = DBGPipeline(logger, hasher, w, reads_lib, dir, threads, "none", "none");
+    dbg.fillAnchors(w, logger, threads); // Creates index of edge k-mers
+    size_t extension_size = 100000;
+    ReadLogger readLogger(threads, dir/"read_log.txt");
+    RecordStorage readStorage(dbg, 0, extension_size, threads, readLogger, true, false, false); // Structure for read alignments
+    io::SeqReader reader(reads_lib); // Reader that can read reads from file
+    readStorage.fill(reader.begin(), reader.end(), dbg, w + k - 1, logger, threads); // Align reads to the graph
+    std::experimental::filesystem::path subdir = dir / "subdatasets";
+    recreate_dir(subdir);
+    std::vector<Subdataset> subdatasets;
+    GraphAlignmentStorage storage(dbg);
+    for(StringContig stringContig : io::SeqReader(ref_lib)) {
+        storage.fill(stringContig.makeContig());
+    }
+    std::vector<dbg::Component> components = dbg::LengthSplitter(unique_threshold).splitGraph(dbg); // Split graph into components
+    subdatasets = oneline::initialize<Subdataset>(components); // Create subdatasets corresponding to components
+    FillSubdatasets(subdatasets, {&readStorage}, true); // Assign reads to datasets
+
+    // New work: identify "high-coverage" edges, and remove the reads covering
+    // these edges. Save the removed reads to a separate file for later use.
+    std::ofstream badreados;
+    badreados.open(dir / "hicov-edge-reads.fasta");
+    for (AlignedRead &read : readStorage) {
+        dbg::CompactPath &cPath = read.path;
+        dbg::GraphAlignment aln = cPath.getAlignment();
+        dbg::Path path = cPath.getPath();
+        for (dbg::Edge *edge : path) {
+            if (edge -> getCoverage() >= hicov) {
+                // Write out the read to a file
+                badreados << ">" << read.id << "\n" << aln.Seq() << "\n";
+                // Mark the read for removal: this doesn't actually remove it
+                // yet -- we'll do that when we call applyCorrections() later
+                read.invalidate();
+                // Now that we know that this read covers a high-coverage edge,
+                // we know that this read is "bad" and can be removed. So we're
+                // done here.
+                break;
+            }
+        }
+    }
+    badreados.close();
+
+    // Actually remove the invalidated reads
+    readStorage.applyCorrections(logger, threads);
+
+    // Now that we've removed certain reads, we will modify the graph
+    // accordingly: remove all edges that are now uncovered
+    std::vector<RecordStorage *> storages = {&readStorage};
+    RemoveUncovered(logger, threads, dbg, storages);
+
+    // From here, write out the subdatasets (components) like normal
+    size_t cnt = 0;
+    for(const Subdataset &subdataset: subdatasets) {
+        logger.info() << "Printing subdataset " << cnt << " " << subdataset.id << ":";
+        for(dbg::Vertex &v : subdataset.component.verticesUnique()) {
+            logger << " " << v.getShortId();
+        }
+        logger << "\n";
+        std::string name = itos(cnt);
+        if(!subdataset.id.empty())
+            name += "_" + name;
+        subdataset.Save(subdir / name, storage.labeler() + readStorage.labeler());
+        cnt++;
+    }
+    return 0;
+}
